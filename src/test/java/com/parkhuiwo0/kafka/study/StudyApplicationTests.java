d4045f46a9b3615ebc371627f9793b66af2e70b9
hilda <kimguihee7@gmail.com>
2020-11-25 15:47:38 +0900

    init project

---
+++ src/test/java/com/parkhuiwo0/kafka/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.kafka.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
